c8fc7ac68a5a43a2c6e1eff766ab8f4f1da617d5
leonardo maia pugliese <leo.pug@hotmail.com>
2020-04-14 09:08:50 -0300

    ui bug fix - navbar not show when swipe right

---
+++ ProjectGitHubApi/Model/Follower.swift
@@ -0,0 +1,9 @@
+//
+//  Follower.swift
+//  ProjectGitHubApi
+//
+//  Created by Ana Caroline de Souza on 13/04/20.
+//  Copyright © 2020 Leo Corp. All rights reserved.
+//
+
+import Foundation
---
+++ ProjectGitHubApi/Model/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  ProjectGitHubApi
+//
+//  Created by Ana Caroline de Souza on 13/04/20.
+//  Copyright © 2020 Leo Corp. All rights reserved.
+//
+
+import Foundation
