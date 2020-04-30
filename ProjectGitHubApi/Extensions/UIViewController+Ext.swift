36db871b61456441aa3734cadabe01a535519eb2
leonardo maia pugliese <leo.pug@hotmail.com>
2020-04-30 17:44:09 -0300

    loading screen added

--- ProjectGitHubApi/Extensions/UIViewController+Ext.swift
+++ ProjectGitHubApi/Extensions/UIViewController+Ext.swift
@@ -1,16 +1,49 @@
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
     
     func presentGHAAlertOnMainThread(title: String, message: String, buttonTitle: String) {
-        
         DispatchQueue.main.async {
             let alertVC = GHAAlertVC(title: title, message: message, buttonTitle: buttonTitle)
             alertVC.modalPresentationStyle = .overFullScreen
             alertVC.modalTransitionStyle = .crossDissolve
             self.present(alertVC, animated: true)
         }
+    }
+    
+    func showLoadingView() {
         
+        containerView = UIView(frame: view.bounds) //fill the whole screen
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.30) {
+            containerView.alpha = 0.8
+        }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func dissmissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
+        }
     }
     
 }
