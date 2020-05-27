3492f07e850212ff2b3ee7637b20027d1db27a31
leonardo maia pugliese <leo.pug@hotmail.com>
2020-05-27 17:28:53 -0300

    favoritecell custom cell done

---
+++ ProjectGitHubApi/Custom Views/Cells/FavoriteCell.swift
@@ -0,0 +1,57 @@
+//
+//  FavoriteCell.swift
+//  ProjectGitHubApi
+//
+//  Created by Ana Caroline de Souza on 27/05/20.
+//  Copyright © 2020 Leo Corp. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+    static let reuseID = "favoriteCell"
+    
+    let avatarImageView = GHAAvatarImageView(frame: .zero)
+    let usernameLabel = GHATitleLable(textAlignment: .left, fontSize: 26)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        accessoryType = .disclosureIndicator
+        let padding : CGFloat = 12
+        NSLayoutConstraint.activate([
+            
+            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            
+            usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
+            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 40)
+            
+        ])
+    }
+    
+    func set(favorite: Follower){
+        usernameLabel.text = favorite.login
+        NetworkManager.shared.downloadImage(from: favorite.avatarUrl) { [weak self] image in
+            DispatchQueue.main.async {
+                self?.avatarImageView.image = image
+            }
+        }
+        
+    }
+}
