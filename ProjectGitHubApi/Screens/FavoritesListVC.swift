9952f54d042468e4b64f8757b2314942414382fc
leonardo maia pugliese <leo.pug@hotmail.com>
2020-05-29 09:00:59 -0300

    swipe to delete done

--- ProjectGitHubApi/Screens/FavoritesListVC.swift
+++ ProjectGitHubApi/Screens/FavoritesListVC.swift
@@ -62,9 +62,34 @@ extension FavoritesListVC: UITableViewDelegate, UITableViewDataSource {
         return favorites.count
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let favorite = favorites[indexPath.row]
+        let destVC = FollowerListVC()
+        destVC.username = favorite.login
+        destVC.title = favorite.login
+        
+        navigationController?.pushViewController(destVC, animated: true)
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseID) as! FavoriteCell
         cell.set(favorite: favorites[indexPath.row])
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        let favorite = favorites[indexPath.row]
+        
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .middle)
+        PersistanceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else { return }
+            guard let error = error else {
+            return
+            }
+            self.presentGHAAlertOnMainThread(title: "Unable to Remove", message: error.rawValue, buttonTitle: "Ok")
+        }
+    }
+    
 }
