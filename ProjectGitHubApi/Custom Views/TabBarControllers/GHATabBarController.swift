c76c21a006b880f3fe199dac66779774147f57a9
leonardo maia pugliese <leo.pug@hotmail.com>
2020-06-02 16:34:00 -0300

    scene delegate cleanup

---
+++ ProjectGitHubApi/Custom Views/TabBarControllers/GHATabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  GHATabBarController.swift
+//  ProjectGitHubApi
+//
+//  Created by Ana Caroline de Souza on 02/06/20.
+//  Copyright © 2020 Leo Corp. All rights reserved.
+//
+
+import UIKit
+
+class GHATabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
